d7895f84fd949eed3ba8f660852ab1a7f6dce22b
takara2314 <takara2314_TK@outlook.jp>
2022-03-27 09:28:42 +0900

    Add: cors setting

--- main.go
+++ main.go
@@ -7,25 +7,35 @@ import (
 	"sailing-assist-mie-api/race"
 	"sailing-assist-mie-api/user"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	route := gin.Default()
+	router := gin.Default()
 
-	device.Register(route.Group("/device"))
+	// CORS settings
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowOrigins = []string{"http://localhost:3000", "https://sailing-assist-mie-manage.herokuapp.com"}
+	router.Use(cors.New(corsConfig))
 
-	user.Register(route.Group("/user"))
-	route.POST("/users", user.UsersPOST)
+	// Device API
+	device.Register(router.Group("/device"))
 
-	race.Register(route.Group("/race"))
-	route.GET("/races", race.RacesGET)
-	route.POST("/races", race.RacesPOST)
-	route.GET("/racing/:id", race.RacingWS)
+	// User API
+	user.Register(router.Group("/user"))
+	router.POST("/users", user.UsersPOST)
+
+	// Race API and Race Socket
+	race.Register(router.Group("/race"))
+	router.GET("/races", race.RacesGET)
+	router.POST("/races", race.RacesPOST)
+	router.GET("/racing/:id", race.RacingWS)
 	go race.AutoRooming()
 
-	group.Register(route.Group("/group"))
-	route.POST("/groups", group.GroupsPOST)
+	// Group API
+	group.Register(router.Group("/group"))
+	router.POST("/groups", group.GroupsPOST)
 
-	route.Run(":" + os.Getenv("PORT"))
+	router.Run(":" + os.Getenv("PORT"))
 }
