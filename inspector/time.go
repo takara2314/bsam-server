a42b8344cb92908f0d71068bb4cb1d0514c11228
takara2314 <takara2314_TK@outlook.jp>
2022-05-05 13:27:31 +0900

    Improve: ignore decimal fractions on time

--- inspector/time.go
+++ inspector/time.go
@@ -1,7 +1,13 @@
 package inspector
 
-import "time"
+import (
+	"strings"
+	"time"
+)
 
 func ParseTimestamp(value string) (time.Time, error) {
+	if strings.Contains(value, ".") {
+		value = strings.Split(value, ".")[0]
+	}
 	return time.Parse("2006-01-02 15:04:05", value)
 }
