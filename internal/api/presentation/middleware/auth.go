452ccdcc4152c56274fc6770b6dad37dc8e3b88b
takara2314 <hamaguchi_takara@cyberagent.co.jp>
2024-08-05 08:02:34 +0900

    feat: トークン認証用のミドルウェアを実装

---
+++ internal/api/presentation/middleware/auth.go
@@ -0,0 +1,60 @@
+package middleware
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/takara2314/bsam-server/internal/game/common"
+	"github.com/takara2314/bsam-server/pkg/auth"
+)
+
+const (
+	bearerPrefix       = "Bearer "
+	bearerPrefixLength = len(bearerPrefix)
+)
+
+func AuthToken() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		value := c.GetHeader("Authorization")
+
+		if value == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": "authorization header is required",
+			})
+			return
+		}
+
+		token, err := extractTokenFromAuthHeader(value)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		assocID, err := auth.ParseJWT(
+			token,
+			common.Env.JWTSecretKey,
+		)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": "token is invalid",
+			})
+			return
+		}
+
+		c.Set("assoc_id", assocID)
+	}
+}
+
+func extractTokenFromAuthHeader(value string) (string, error) {
+	if value == "" {
+		return "", errors.New("authorization header is required")
+	}
+	if !strings.HasPrefix(value, bearerPrefix) {
+		return "", errors.New("authorization header must start with Bearer")
+	}
+	return strings.TrimPrefix(value, bearerPrefix), nil
+}
