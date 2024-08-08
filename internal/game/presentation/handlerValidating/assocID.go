0d99e598442a5ed6133df8a1bb7f5be420c3afce
takara2314 <hamaguchi_takara@cyberagent.co.jp>
2024-08-09 07:01:05 +0900

    refactor(game): ハブ作成の部分の可読性を向上

--- internal/game/presentation/handlerValidating/assocID.go
+++ internal/game/presentation/handlerValidating/assocID.go
@@ -14,22 +14,37 @@ func AssocIDWS(c *gin.Context) {
 	var err error
 	assocID := c.Param("assocID")
 
-	if _, exist := common.Hubs[assocID]; !exist {
-		common.Hubs[assocID], err = createNewHub(c, assocID)
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
-				"error": "assoc_id is not found",
-			})
-			return
-		}
+	hub, err := findOrCreateHub(c, assocID)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+			"error": "assoc_id is not found",
+		})
+		return
 	}
 
 	conn, err := racehub.Upgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "failed to upgrade connection",
+		})
 		return
 	}
 
-	common.Hubs[assocID].Register(conn)
+	hub.Register(conn)
+}
+
+func findOrCreateHub(c *gin.Context, assocID string) (*racehub.Hub, error) {
+	if hub, exist := common.Hubs[assocID]; exist {
+		return hub, nil
+	}
+
+	hub, err := createNewHub(c, assocID)
+	if err != nil {
+		return nil, err
+	}
+
+	common.Hubs[assocID] = hub
+	return hub, nil
 }
 
 func createNewHub(ctx context.Context, assocID string) (*racehub.Hub, error) {
