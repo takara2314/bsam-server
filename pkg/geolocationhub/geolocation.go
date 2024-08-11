5b35dda7bcf6076b07de0fa87c22cb34d6c13f9a
takara2314 <hamaguchi_takara@cyberagent.co.jp>
2024-08-11 21:58:50 +0900

    refactor(geolocationhub): ファイルを分離

--- pkg/geolocationhub/geolocation.go
+++ pkg/geolocationhub/geolocation.go
@@ -5,37 +5,9 @@ import (
 	"log/slog"
 	"time"
 
-	"cloud.google.com/go/firestore"
 	"github.com/samber/oops"
 	repoFirestore "github.com/takara2314/bsam-server/pkg/infrastructure/repository/firestore"
 )
-
-type Hub struct {
-	AssocID         string
-	FirestoreClient *firestore.Client
-}
-
-type Geolocation struct {
-	DeviceID              string
-	Latitude              float64
-	Longitude             float64
-	AltitudeMeter         float64
-	AccuracyMeter         float64
-	AltitudeAccuracyMeter float64
-	Heading               float64
-	SpeedMeterPerSec      float64
-	RecordedAt            time.Time
-}
-
-func NewHub(
-	AssocID string,
-	firestoreClient *firestore.Client,
-) *Hub {
-	return &Hub{
-		AssocID:         AssocID,
-		FirestoreClient: firestoreClient,
-	}
-}
 
 func (h *Hub) StoreGeolocation(
 	ctx context.Context,
