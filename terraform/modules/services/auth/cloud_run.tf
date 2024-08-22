ef4eefb7b54667f54fad9a85d221bdadbca5c277
takara2314 <hamaguchi_takara@cyberagent.co.jp>
2024-08-23 01:34:54 +0900

    fix(iac): 未認証のアクセスを許可するように

--- terraform/modules/services/auth/cloud_run.tf
+++ terraform/modules/services/auth/cloud_run.tf
@@ -31,4 +31,17 @@ resource "google_cloud_run_v2_service" "auth_service" {
       }
     }
   }
+
+  # 未認証のアクセスを許可する設定を追加
+  labels = {
+    "cloud.googleapis.com/allow-unauthenticated" = "true"
+  }
+}
+
+resource "google_cloud_run_service_iam_member" "auth_service_public" {
+  location = google_cloud_run_v2_service.auth_service.location
+  project  = google_cloud_run_v2_service.auth_service.project
+  service  = google_cloud_run_v2_service.auth_service.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
 }
