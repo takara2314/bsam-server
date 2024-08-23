5fbefab8f939504f6eb5ed10dd36103bd3b0dce7
takara2314 <hamaguchi_takara@cyberagent.co.jp>
2024-08-23 09:46:09 +0900

    feat(iac): アクセスがないときはインスタンスを止めるように

--- terraform/modules/services/auth/cloud_run.tf
+++ terraform/modules/services/auth/cloud_run.tf
@@ -28,6 +28,7 @@ resource "google_cloud_run_v2_service" "auth_service" {
           memory = "512Mi"
         }
         startup_cpu_boost = true
+        cpu_idle          = false
       }
     }
   }
